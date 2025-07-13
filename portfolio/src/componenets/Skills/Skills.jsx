96ce7f50596ef35dbc46f6b681e3d5310b113120
Hrishikesh Sarma <hrishikeshsarma4444@gmail.com>
2025-07-13 08:51:59 +0530

    added project

--- portfolio/src/componenets/Skills/Skills.jsx
+++ portfolio/src/componenets/Skills/Skills.jsx
@@ -8,6 +8,8 @@ import {
   SiNodedotjs,
   SiExpress,
   SiMongodb,
+  SiNextdotjs,
+  SiTypescript 
 } from "react-icons/si";
 import {
   SiRedux,
@@ -17,39 +19,30 @@ import {
   SiMysql,
   SiBootstrap,
   SiTailwindcss,
-
-
 } from "react-icons/si";
+import { TbBrandTypescript } from "react-icons/tb";
 
 import { DiGit, DiGithubBadge } from "react-icons/di";
 
 const Skills = () => {
-  const languageSkills = [
-    { name: "C", icon: <SiC /> },
+  const allSkills = [
+    { name: "", icon: <SiC /> },
     { name: "C++", icon: <SiCplusplus /> },
     { name: "JavaScript", icon: <SiJavascript /> },
-  ];
-  const frontendSkills = [
-    { name: "React Js", icon: <FaReact /> },
+    { name: "Typescript", icon:<SiTypescript  /> },
     { name: "HTML", icon: <SiHtml5 /> },
     { name: "CSS", icon: <SiCss3 /> },
-    { name: "JavaScript", icon: <SiJavascript /> },
+    { name: "React Js", icon: <FaReact /> },
+    { name: "Next Js", icon: <SiNextdotjs /> },
     { name: "Redux", icon: <SiRedux /> },
     { name: "Bootsrap", icon: <SiBootstrap /> },
-    { name: "Tailwind CSS", icon: <SiTailwindcss/> },
-  ];
-  const backendSkills = [
+    { name: "Tailwind CSS", icon: <SiTailwindcss /> },
     { name: "Node.js", icon: <SiNodedotjs /> },
     { name: "Express.js", icon: <SiExpress /> },
     { name: "MongoDB", icon: <SiMongodb /> },
-  ];
-  const othersSkills = [
     { name: "Git", icon: <DiGit /> },
     { name: "GitHub", icon: <DiGithubBadge /> },
     { name: "Mysql", icon: <SiMysql /> },
-    
-
-
   ];
 
   return (
@@ -60,41 +53,11 @@ const Skills = () => {
           Here are some of my skills on which I have been working on.
         </p>
         <div className="skills-container">
-          <div className="skills-box">
-            <h3 className="frontend-title">Language</h3>
-            {languageSkills.map((skill, index) => (
-              <div className="skill-card" key={index}>
+          {allSkills.map((skill, index) => (
+            <div className="skills-box" key={index}>
                 {skill.icon} {skill.name}
-              </div>
-            ))}
-          </div>
-
-          <div className="skills-box">
-            <h3 className="frontend-title">Frontend</h3>
-            {frontendSkills.map((skill, index) => (
-              <div className="skill-card" key={index}>
-                {skill.icon} {skill.name}
-              </div>
-            ))}
-          </div>
-
-          <div className="skills-box">
-            <h3 className="frontend-title">Backend</h3>
-            {backendSkills.map((skill, index) => (
-              <div className="skill-card" key={index}>
-                {skill.icon} {skill.name}
-              </div>
-            ))}
-          </div>
-
-          <div className="skills-box">
-            <h3 className="frontend-title">Others</h3>
-            {othersSkills.map((skill, index) => (
-              <div className="skill-card" key={index}>
-                {skill.icon} {skill.name}
-              </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
